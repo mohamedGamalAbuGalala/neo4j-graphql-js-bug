43671e8d2d519da82f031163b18e710fe5e0f1cf
Mohamed Gamal <mohamed.abugalala@gmail.com>
2020-02-17 18:56:32 +0200

    Init

---
+++ index.js
@@ -0,0 +1,45 @@
+const { ApolloServer } = require("apollo-server");
+const {
+  makeAugmentedSchema
+} = require("neo4j-graphql-js");
+const neo4j = require("neo4j-driver");
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = `
+type Query {
+  rootQuery: String @cypher(statement: "WITH 'Wooorking 🐈 🐈 🐈 🐈' AS value RETURN value")
+ }
+
+ extend type Query {
+  extendQuery: String @cypher(statement: "WITH 'NOT WORKING 😞 😞 😞 😞' AS value RETURN value")
+ }
+
+ type Movie {
+  title: String
+  movieTestBase: String @cypher(statement: "WITH 'Wooorking 🐈 🐈 🐈 🐈' AS value RETURN value")
+ }
+
+ extend type Movie {
+  movieTestExtend: String @cypher(statement: "WITH 'Wooorking 🐈 🐈 🐈 🐈' AS value RETURN value")
+ }
+`;
+
+// Generate executable schema with auto-generated resolvers
+const schema = makeAugmentedSchema({ typeDefs });
+
+// Instantiate a Neo4j database driver
+const driver = neo4j.driver(
+  "bolt://localhost:7687",
+  neo4j.auth.basic("neo4j", "test")
+);
+
+// Create a new ApolloServer, injecting the database driver
+// into the context
+const server = new ApolloServer({
+  context: { driver },
+  schema
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
